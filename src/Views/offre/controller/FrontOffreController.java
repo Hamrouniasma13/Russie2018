745b1e4607697fcf4c1f221ba11bb232e1e05d46
Asma <>
2018-04-08 21:01:26 +0100

    intitialize project

---
+++ src/Views/offre/controller/FrontOffreController.java
@@ -0,0 +1,60 @@
+
+package Views.offre.controller;
+
+import Views.agence.controller.*;
+import Services.AgenceService;
+import entities.Agence;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class FrontOffreController implements Initializable {
+
+    @FXML
+    private VBox container;
+
+    @FXML
+    private TextField filterField;
+
+    @FXML
+    private Button rechercheBtn;
+    
+    private String motCle="";
+
+    
+    AgenceService AS;
+    ObservableList<Agence> agences; 
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        AS= new AgenceService();
+        rechercheBtn.setOnMouseClicked(e->{
+            motCle = filterField.getText();
+            initData();
+        });
+        initData();
+    }  
+    
+    public void initData(){
+        agences =  FXCollections.observableArrayList(AS.getAll(motCle));
+        container.getChildren().removeIf(e -> true);
+        
+        agences.forEach((Agence a) -> {
+            AgenceController ac = new AgenceController();
+            ac.setData(a);
+            container.setSpacing(15);
+            container.getChildren().add(ac);
+        });
+    }
+    
+}
